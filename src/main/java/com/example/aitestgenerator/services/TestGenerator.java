e138501a4d34def7fa2f121da9783d75c3203cdb
dmnvch98 <dmnvch.evg@gmail.com>
2023-10-18 18:56:14 +0400

    The multithreading was added to speed up the test generating

--- src/main/java/com/example/aitestgenerator/services/TestGenerator.java
+++ src/main/java/com/example/aitestgenerator/services/TestGenerator.java
@@ -1,20 +1,24 @@
 package com.example.aitestgenerator.services;
 
+import com.example.aitestgenerator.models.Question;
 import com.example.aitestgenerator.models.Test;
 import com.example.aitestgenerator.models.Text;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatCompletionResult;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.service.OpenAiService;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static com.example.aitestgenerator.utils.Utils.countTokens;
 import static com.example.aitestgenerator.utils.Utils.readFileContents;
@@ -26,23 +30,60 @@ public class TestGenerator {
     private final ObjectMapper objectMapper;
     private final OpenAiService openAiService;
 
-    @SneakyThrows
     public Test start(Text text) {
-        log.info("Starting test generation. Text id: {}, User id: {}", text.getId(), text.getUserId());
-
         List<ChatMessage> messages = new ArrayList<>();
+        List<List<String>> questionGroups = splitQuestionsIntoGroups(text, messages);
 
-        log.info("Generating Questions. Text id: {}, User id: {}", text.getId(), text.getUserId());
+        List<CompletableFuture<Test>> futureTests = new ArrayList<>();
+
+        for (List<String> group : questionGroups) {
+            CompletableFuture<Test> futureTest = generateTestAsync(group, text, messages);
+            futureTests.add(futureTest);
+        }
+
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futureTests.toArray(new CompletableFuture[0]));
+
+        return allOf.thenApply(ignored -> {
+            List<Question> generatedQuestions = new ArrayList<>();
+            for (CompletableFuture<Test> futureTest : futureTests) {
+                generatedQuestions.addAll(futureTest.join().getQuestions());
+            }
+
+            return Test.builder()
+                .title(text.getTitle())
+                .questions(generatedQuestions)
+                .userId(text.getUserId())
+                .build();
+        }).join();
+    }
+
+    private CompletableFuture<Test> generateTestAsync(List<String> questionGroup, Text text, List<ChatMessage> messages) {
+        return CompletableFuture.supplyAsync(() -> {
+            log.info("Generating Test. Text id: {}, User id: {}. Questions: {}", text.getId(), text.getUserId(), questionGroup);
+            String testJson = generateData(new ArrayList<>(messages), readFileContents("ai_prompts/generate_test.txt"), questionGroup.toString(), text);
+            try {
+                return objectMapper.readValue(testJson, Test.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private List<List<String>> splitQuestionsIntoGroups(Text text, List<ChatMessage> messages) {
+        log.info("Generating questions for text. Text id: {}, User id: {}", text.getId(), text.getUserId());
         String questions = generateData(messages, readFileContents("ai_prompts/generate_questions.txt"), text.toString(), text);
+        return parseQuestionGroups(questions, text);
+    }
 
-        log.info("Generating Test. Text id: {}, User id: {}", text.getId(), text.getUserId());
-        String testJson = generateData(messages, readFileContents("ai_prompts/generate_test.txt"), questions, text);
+    private List<List<String>> parseQuestionGroups(String questions, Text text) {
+        log.info("Parsing questions from string. Text id: {}, User id: {}", text.getId(), text.getUserId());
+        List<String> questionList = Arrays.asList(questions.split("\n"));
+        int groupSize = 3;
 
-        Test test = objectMapper.readValue(testJson, Test.class);
-
-        log.info("Test generation completed. Text id: {}, User id: {}", text.getId(), text.getUserId());
-
-        return test;
+        return IntStream.range(0, questionList.size())
+            .filter(i -> i % groupSize == 0)
+            .mapToObj(i -> questionList.subList(i, Math.min(i + groupSize, questionList.size())))
+            .collect(Collectors.toList());
     }
 
     private String generateData(List<ChatMessage> messages, String request, String content, Text text) {
